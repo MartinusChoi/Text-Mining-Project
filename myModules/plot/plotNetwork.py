38c6deaf3e27db6030b54fc9a5a5f9ee47d9abc3
youngminChoi <martinus.choi@gmail.com>
2021-11-26 17:42:45 +0900

    chosun Network

--- myModules/plot/plotNetwork.py
+++ myModules/plot/plotNetwork.py
@@ -9,6 +9,7 @@ from tqdm.notebook import tqdm
 import pandas as pd
 import operator
 import numpy as np
+from nltk import bigrams, ConditionalFreqDist
 
 class vocaDict:
     def __init__(self):
@@ -50,96 +51,132 @@ class vocaDict:
 ################## Network #########################
 
 class Network:
-    def calc_properties(self, edge_num, data):
-        self.edge_num = edge_num
-        self.data = data
-        self.max_freq = -1
-        self.quartile = 0
+    def __init__(self, data, num_node=30, keywords=[]):
+        bgrams = [bigrams(tokens) for tokens in data]
 
-        G_centrality = nx.Graph()
-
-        for idx in range(self.edge_num):
-            G_centrality.add_edge(self.data['word1'][idx], self.data['word2'][idx], weight=int(self.data['freq'][idx]))
-            if self.max_freq < int(self.data['freq'][idx]): self.max_freq = int(self.data['freq'][idx])
+        token = []
+        for bgram in bgrams:
+            token += ([x for x in bgram])
         
-        self.quartile = self.max_freq / 4
-        
-        self.dgr = nx.degree_centrality(G_centrality)
-        self.cls = nx.closeness_centrality(G_centrality)
-        self.btw = nx.betweenness_centrality(G_centrality)
-        self.eig = nx.eigenvector_centrality(G_centrality)
-        self.pgr = nx.pagerank(G_centrality)
+        cfd = ConditionalFreqDist(token)
+        freq_mat = []
 
-        self.dgr = sorted(self.dgr.items(), key=operator.itemgetter(1), reverse=True)
-        self.cls = sorted(self.cls.items(), key=operator.itemgetter(1), reverse=True)
-        self.btw = sorted(self.btw.items(), key=operator.itemgetter(1), reverse=True)
-        self.eig = sorted(self.eig.items(), key=operator.itemgetter(1), reverse=True)
-        self.pgr = sorted(self.pgr.items(), key=operator.itemgetter(1), reverse=True)
+        for i in tqdm(cfd.keys()):
+            temp = []
+            for j in cfd.keys():
+                temp.append(cfd[i][j])
+            freq_mat.append(temp)
+        freq_mat = np.array(freq_mat)
+        freq_df = pd.DataFrame(freq_mat, index=cfd.keys(), columns=cfd.keys())
+
+        if len(keywords) == 0:
+            co_occurrence_dict = {}
+            for idx in range(len(freq_df)):
+                co_occurrence_dict[str(idx)] = freq_df.iloc[idx, :].max()
+            sorted_co_occurrence_dict = sorted(co_occurrence_dict.items(), key = lambda item: item[1], reverse=True)
+            top_N = sorted_co_occurrence_dict[:num_node]
+            top_N_idx = []
+            for idx, _ in top_N:
+                top_N_idx.append(int(idx))
+        
+            self.G = nx.from_pandas_adjacency(freq_df.iloc[top_N_idx, :])
+        else : 
+            valid_idx = keywords.copy()
+
+            tmp = freq_df.loc[keywords, :]
+
+            for idx in keywords:
+                valid_idx.extend(tmp.loc[idx, tmp.loc[idx, :] > 0].index)
+
+            co_occurrence_dict = {}
+            for idx in valid_idx:
+                co_occurrence_dict[idx] = freq_df.loc[idx, :].max()
+            sorted_co_occurrence_dict = sorted(co_occurrence_dict.items(), key = lambda item: item[1], reverse=True)
+            top_N = sorted_co_occurrence_dict[:num_node]
+            top_N_idx = []
+            for idx, _ in top_N:
+                top_N_idx.append(idx)
+
+            self.G = nx.from_pandas_adjacency(freq_df.loc[top_N_idx, :])
+
+        dgr = nx.degree_centrality(self.G)
+        self.dgr = sorted(dgr.items(), key=operator.itemgetter(1), reverse=True)
+
+        cls = nx.closeness_centrality(self.G)
+        self.cls = sorted(cls.items(), key=operator.itemgetter(1), reverse=True)
+
+        btw = nx.betweenness_centrality(self.G)
+        self.btw = sorted(btw.items(), key=operator.itemgetter(1), reverse=True)
+
+        eig = nx.eigenvector_centrality(self.G)
+        self.eig = sorted(eig.items(), key=operator.itemgetter(1), reverse=True)
     
-    def plot(self, centrality='degree', title='Network Web Analysis', root='./'):
-        G = nx.Graph()
-
-        # node 추가
+    def save_centrality_table(self, table_len=10, title='centrality_table', root='./'):
+        dgr_df = pd.DataFrame(self.dgr)
+        cls_df = pd.DataFrame(self.cls)
+        btw_df = pd.DataFrame(self.btw)
+        eig_df = pd.DataFrame(self.eig)
+        centrality_df = pd.concat([dgr_df, cls_df, btw_df, eig_df], axis=1)
+        centrality_df.columns = ['Word', 'Degree Centrality', 'Word', 'Closeness Centrality', \
+            'Word', 'Betweenness Centrality', 'Word', 'Eigenvector Centrality']
+        centrality_df[:table_len].to_csv(root+title+'.csv')
+        return centrality_df[:table_len]
+    
+    def plot(self, centrality='degree', title='Network-Analysis', root='./'):
         if centrality == 'degree':
-            for idx in range(len(self.dgr)):
-                G.add_node(self.dgr[idx][0], weight=self.dgr[idx][1])
+            title = title + '-Degree'
+            centrality_value = self.dgr
         elif centrality == 'closeness':
-            for idx in range(len(self.cls)):
-                G.add_node(self.cls[idx][0], weight=self.cls[idx][1])
+            title = title + '-Closeness'
+            centrality_value = self.cls
         elif centrality == 'betweenness':
-            for idx in range(len(self.btw)):
-                G.add_node(self.btw[idx][0], weight=self.btw[idx][1])
+            title = title + '-Betweenness'
+            centrality_value = self.btw
         elif centrality == 'eigenvector':
-            for idx in range(len(self.eig)):
-                G.add_node(self.eig[idx][0], weight=self.eig[idx][1])
-        elif centrality == 'pagerank':
-            for idx in range(len(self.pgr)):
-                G.add_node(self.pgr[idx][0], weight=self.pgr[idx][1])
-        
-        # edge 추가
-        for idx in range(self.edge_num):
-            G.add_edge(self.data['word1'][idx], self.data['word2'][idx], weight=int(self.data['freq'][idx]))
-        
-        # 토폴로지 형태 정의
-        # pos = graphviz_layout(G, prog='neato')
-        pos = graphviz_layout(G, prog='twopi')
+            title = title + '-Eigenvector'
+            centrality_value = self.eig
 
-        # Network 시각화
+        for token, central in centrality_value:
+            self.G.nodes[token]['weight'] = central
+
         fig = plt.figure(figsize=(30, 20))
         plt.margins(x=0.1, y=0.2)
         ax = fig.gca()
 
-        # figure에 node 그리기
+        # edge와 node의 label 부여하기
+        edge_labels = dict(((u, v), d["weight"]) for u, v, d in self.G.edges(data=True))
+        node_labels = dict((token, token) for token, _ in self.G.nodes(data=True))
+        pos = graphviz_layout(self.G, prog='neato')
+        nx.draw_networkx_labels(self.G, pos, labels=node_labels)
+
+        # Node에 centrality에 따라 weight 부여
         Blues_modified = cm.get_cmap('Blues', 256)
         newcmp = ListedColormap(Blues_modified(np.linspace(0.2, 0.8, 4)))
-        nc = nx.draw_networkx_nodes(G, pos, node_size=[node[1]['weight']*20000 for node in G.nodes(data=True)], \
-            node_color=[node[1]['weight'] for node in G.nodes(data=True)], cmap=newcmp,
-            node_shape='o', alpha=0.9, linewidths=0.4, edgecolors='#000000')
+        nc = nx.draw_networkx_nodes(self.G, pos, 
+            node_size=[node[1]["weight"]*20000 for node in self.G.nodes(data=True)],
+            node_color=[node[1]['weight'] for node in self.G.nodes(data=True)], 
+            cmap=newcmp, node_shape='o', alpha=0.9, linewidths=0.4, edgecolors='#000000')
 
-        # figure에 label 그리기
-        nx.draw_networkx_labels(G, pos=pos, font_size=16, alpha=0.7, font_color='black', font_weight='bold')
-
-        # figure에 edge 그리기
+        # Edge에 co-occurrence에 따라 weight 부여
         Greys_modified = cm.get_cmap('Greys', 256)
         newcmp = ListedColormap(Greys_modified(np.linspace(0.2, 1.0, 4)))
-        ec = nx.draw_networkx_edges(G, pos, edge_color=[edge[2]['weight'] for edge in G.edges(data=True)], \
-            edge_cmap=newcmp, style='solid', width=2, connectionstyle='arc3,rad=-0.3')
-        
-        # title 지정
+        ec = nx.draw_networkx_edges(self.G, pos, 
+            edge_color=[edge[2]['weight'] for edge in self.G.edges(data=True)],
+            edge_cmap=newcmp, style='solid', width=2)
+
+        # Title 설정
         plt.title(title, fontsize=25)
-        # axis 선 안보이게 설정
         plt.axis('off')
 
-        # color bar 추가
-        axins = inset_axes(ax,\
-            width='1%',
-            height='30%',
-            loc='center right',
-            borderpad=0)
+        axins = inset_axes(ax,
+                    width='1%',
+                    height='30%',
+                    loc='center right',
+                    borderpad=0)
         cbar = plt.colorbar(nc, cax=axins)
         cbar.ax.set_ylabel('Centrality', rotation=270, fontsize=12, labelpad=15)
 
-        axins = inset_axes(ax, \
+        axins = inset_axes(ax,
             width='1%',
             height='30%',
             loc='center left',
@@ -147,7 +184,5 @@ class Network:
         cbar = plt.colorbar(ec, cax=axins)
         cbar.ax.set_ylabel('Word Pair Freqency', rotation=270, fontsize=12, labelpad=15)
 
-        # figure 저장
         plt.savefig(root+title+'.png')
-        # figure 출력
         plt.show()
