4ba3a432d3482036c48f9f2698bc41feb4583a1b
youngminChoi <martinus.choi@gmail.com>
2021-12-13 02:55:58 +0900

    TT Network Analysis

--- myModules/plot/plotNetwork.py
+++ myModules/plot/plotNetwork.py
@@ -9,9 +9,10 @@ from tqdm.notebook import tqdm
 import pandas as pd
 import operator
 import numpy as np
-from nltk import bigrams, ConditionalFreqDist
+from nltk import bigrams, ConditionalFreqDist, ngrams
 import matplotlib.font_manager as fm
 from matplotlib import rc
+import re
 
 class vocaDict:
     def __init__(self):
@@ -236,19 +237,19 @@ class Network_v2:
 
         relation_list = valid_idx.copy()
 
-        try : relation_list.remove('soviet')
+        try : relation_list.remove(keywords[1])
         except : pass
 
-        try : relation_list.remove('america')
+        try : relation_list.remove(keywords[0])
         except : pass
 
-        relation_map = [('america', 'america'), ('soviet', 'soviet')]
+        relation_map = [(keywords[0], keywords[0]), (keywords[1], keywords[1])]
 
         for row_idx in relation_list:
-            if self.freq_df[row_idx]['america'] > self.freq_df[row_idx]['soviet']:
-                relation_map.append((row_idx, 'america'))
-            elif self.freq_df[row_idx]['america'] < self.freq_df[row_idx]['soviet']:
-                relation_map.append((row_idx, 'soviet'))
+            if self.freq_df[row_idx][keywords[0]] > self.freq_df[row_idx][keywords[1]]:
+                relation_map.append((row_idx, keywords[0]))
+            elif self.freq_df[row_idx][keywords[0]] < self.freq_df[row_idx][keywords[1]]:
+                relation_map.append((row_idx, keywords[1]))
             else : 
                 relation_map.append((row_idx, 'neutral'))
 
@@ -256,9 +257,9 @@ class Network_v2:
         relation_with_america = {}
         relation_with_soviet = {}
         for idx, relation in relation_map:
-            if relation == 'america':
+            if relation == keywords[0]:
                 relation_with_america[idx] = self.freq_df.loc[idx, :].max()
-            elif relation == 'soviet':
+            elif relation == keywords[1]:
                 relation_with_soviet[idx] = self.freq_df.loc[idx, :].max()
 
         sorted_relation_with_america = sorted(relation_with_america.items(), key = lambda item : item[1], reverse=True)
@@ -281,19 +282,19 @@ class Network_v2:
         
         relation_list = self.top_N_idx.copy()
 
-        try : relation_list.remove('soviet')
+        try : relation_list.remove(keywords[1])
         except : pass
 
-        try : relation_list.remove('america')
+        try : relation_list.remove(keywords[0])
         except : pass
 
-        self.relation_map = [('america', 'america'), ('soviet', 'soviet')]
+        self.relation_map = [(keywords[0], keywords[0]), (keywords[1], keywords[1])]
 
         for row_idx in relation_list:
-            if self.freq_df[row_idx]['america'] > self.freq_df[row_idx]['soviet']:
-                self.relation_map.append((row_idx, 'america'))
-            elif self.freq_df[row_idx]['america'] < self.freq_df[row_idx]['soviet']:
-                self.relation_map.append((row_idx, 'soviet'))
+            if self.freq_df[row_idx][keywords[0]] > self.freq_df[row_idx][keywords[1]]:
+                self.relation_map.append((row_idx, keywords[0]))
+            elif self.freq_df[row_idx][keywords[0]] < self.freq_df[row_idx][keywords[1]]:
+                self.relation_map.append((row_idx, keywords[1]))
             else : 
                 self.relation_map.append((row_idx, 'neutral'))
 
@@ -381,6 +382,259 @@ class Network_v2:
             nodelist = [node[0] for node in self.G.nodes(data=True) if node[1]['relation'] == 'soviet'], 
             node_size=[node[1]["weight"]*20000 for node in self.G.nodes(data=True) if node[1]['relation'] == 'soviet'],
             node_color=[node[1]['weight'] for node in self.G.nodes(data=True) if node[1]['relation'] == 'soviet'], 
+            cmap=redscmp, node_shape='o', alpha=0.9, linewidths=0.4, edgecolors='#000000')
+
+        # Edge에 co-occurrence에 따라 weight 부여
+        Greys_modified = cm.get_cmap('Greys', 256)
+        newcmp = ListedColormap(Greys_modified(np.linspace(0.2, 1.0, 4)))
+        ec = nx.draw_networkx_edges(self.G, pos, 
+            edge_color=[edge[2]['weight'] for edge in self.G.edges(data=True)],
+            edge_cmap=newcmp, style='solid', width=2)
+
+        # Title 설정
+        plt.title(title, fontsize=25)
+        plt.axis('off')
+
+        axins = inset_axes(ax,
+                    width='1%',
+                    height='30%',
+                    loc='center right',
+                    borderpad=0)
+        cbar = plt.colorbar(nc_blues, cax=axins)
+        cbar.ax.set_ylabel('Centrality (America)', rotation=270, fontsize=12, labelpad=15)
+
+        axins = inset_axes(ax,
+                    width='1%',
+                    height='30%',
+                    loc='upper right',
+                    borderpad=0)
+        cbar = plt.colorbar(nc_reds, cax=axins)
+        cbar.ax.set_ylabel('Centrality (Soviet)', rotation=270, fontsize=12, labelpad=15)
+
+        axins = inset_axes(ax,
+            width='1%',
+            height='30%',
+            loc='lower right',
+            borderpad=0)
+        cbar = plt.colorbar(ec, cax=axins)
+        cbar.ax.set_ylabel('Word Pair Freqency', rotation=270, fontsize=12, labelpad=15)
+
+        plt.savefig(root+title+'.png')
+        plt.show()
+
+    def save_centrality_table(self, table_len=10, title='centrality_table', root='./'):
+        dgr_df = pd.DataFrame(self.dgr)
+        cls_df = pd.DataFrame(self.cls)
+        btw_df = pd.DataFrame(self.btw)
+        eig_df = pd.DataFrame(self.eig)
+        centrality_df = pd.concat([dgr_df, cls_df, btw_df, eig_df], axis=1)
+        centrality_df.columns = ['Word', 'Degree Centrality', 'Word', 'Closeness Centrality', \
+            'Word', 'Betweenness Centrality', 'Word', 'Eigenvector Centrality']
+        centrality_df[:table_len].to_csv(root+title+'.csv')
+        return centrality_df[:table_len]
+
+########################################################
+########## Network version 3 ########################
+
+class Network_v3:
+    def __init__(self, data, max_node=10, keywords=['america', 'soviet']):
+        self.keywords = keywords
+
+        # ngram의 list 형태로 변환
+        token = self.to_ngram(data=data, n=2)
+
+        # frequency dist 객체를 Pandas dataframe으로 제작
+        self.freq_df = self.gen_frequency_df(token)
+
+        related_with_america, related_with_soviet = self.gen_relation_list(freq_df=self.freq_df, keywords=keywords)
+
+        try : top_N_america = related_with_america[:max_node]
+        except : top_N_america = related_with_america[:]
+
+        try : top_N_soviet = related_with_soviet[:max_node]
+        except : top_N_soviet = related_with_soviet[:]
+
+        top_N = top_N_america.copy()
+        top_N.extend(top_N_soviet)
+        top_N = sorted(top_N, key= lambda item : item[1], reverse=True)
+
+        self.top_N_idx = []
+
+        for idx, _ in top_N:
+            self.top_N_idx.append(idx)
+        
+        self.relation_map = self.gen_relation_map(freq_df=self.freq_df, top_N_idx=self.top_N_idx, keywords=keywords)
+    
+    def to_ngram(self, data, n=2):
+        # token을 bigram으로
+        bgrams = [ngrams(tokens, n) for tokens in data]
+
+        # bigram 객체를 튜플들의 list 형태로 변환
+        token = []
+        for bgram in bgrams:
+            token += ([x for x in bgram])
+        
+        return token
+    
+    def gen_frequency_df(self, ngrams):
+        # ConditionalFreqDist 클래스를 이용하여 bigram으로 부터 frequency dist 계산
+        cfd = ConditionalFreqDist(ngrams)
+        freq_mat = []
+
+        # frequency dist 객체를 Pandas dataframe으로 제작
+        for i in tqdm(cfd.keys(), desc="Generate Frequency Dist"):
+            temp = []
+            for j in cfd.keys():
+                temp.append(cfd[i][j])
+            freq_mat.append(temp)
+        freq_mat = np.array(freq_mat)
+        freq_df = pd.DataFrame(freq_mat, index=cfd.keys(), columns=cfd.keys())
+
+        return freq_df
+    
+    def gen_relation_list(self, freq_df, keywords):
+        # keywords를 포함하여 Network에 이용한 row들의 index저장을 위해 copy
+        relation_list = keywords.copy()
+
+        # keywords에 해당하는 row만을 freq_dist에서 추출
+        tmp = freq_df.loc[keywords, :]
+
+        # 살펴보면서 해당 keywords들과 freq 값이 계산되어있는 column idx들을 valid idx에 추가
+        for idx in keywords:
+            relation_list.extend(tmp.loc[idx, tmp.loc[idx, :] > 0].index)
+
+        # 핵심 keyword는 연관된 token들의 list에서 삭제
+        relation_list = [val for val in relation_list if val != keywords[0]]
+        relation_list = [val for val in relation_list if val != keywords[1]]
+
+        # tuple의 첫번째 원소 : 원래 token
+        # tuple의 두번쨰 원소 : 연관된 keyword
+        relation_map = [(keywords[0], keywords[0]), (keywords[1], keywords[1])]
+
+        # 두가지 keyword 중 더 연관 있는 keyword를 지정하여 tuple 형태로 append
+        for row_idx in relation_list:
+            if self.freq_df[row_idx][keywords[0]] > self.freq_df[row_idx][keywords[1]]:
+                relation_map.append((row_idx, keywords[0]))
+            elif self.freq_df[row_idx][keywords[0]] < self.freq_df[row_idx][keywords[1]]:
+                relation_map.append((row_idx, keywords[1]))
+            else : # 두 keyword 간 연관(동시 출현 빈도)값이 같은 경우
+                relation_map.append((row_idx, 'neutral'))
+
+        relate_with_america = {}
+        relate_with_soviet = {}
+        for idx, relation in relation_map:
+            if relation == keywords[0]:
+                relate_with_america[idx] = freq_df.loc[idx, :].max()
+            elif relation == keywords[1]:
+                relate_with_soviet[idx] = freq_df.loc[idx, :].max()
+
+        sorted_relate_with_america = sorted(relate_with_america.items(), key = lambda item : item[1], reverse=True)
+        sorted_relate_with_soviet = sorted(relate_with_soviet.items(), key = lambda item : item[1], reverse=True)
+
+        return sorted_relate_with_america, sorted_relate_with_soviet
+    
+    def gen_relation_map(self, freq_df, top_N_idx, keywords):
+        relation_list = top_N_idx.copy()
+
+        relation_list = [val for val in relation_list if val != keywords[0]]
+        relation_list = [val for val in relation_list if val != keywords[1]]
+
+        relation_map = [(keywords[0], keywords[0]), (keywords[1], keywords[1])]
+
+        for row_idx in relation_list:
+            if freq_df[row_idx][keywords[0]] > freq_df[row_idx][keywords[1]]:
+                relation_map.append((row_idx, keywords[0]))
+            elif freq_df[row_idx][keywords[0]] < freq_df[row_idx][keywords[1]]:
+                relation_map.append((row_idx, keywords[1]))
+            else : 
+                relation_map.append((row_idx, 'neutral'))
+        
+        return relation_map
+
+    def remove(self, token):
+        return re.sub("[^a-z'\ ]", "", token)
+
+    def remove_dot(self, data):
+        result = []
+
+        for tokens in data:
+            temp = []
+            for token in tokens:
+                if "." in token : temp.append(self.remove(token))
+                else : temp.append(token)
+            result.append(temp)
+        
+        return result
+    
+    def generate_network(self):
+        self.G = nx.from_pandas_adjacency(self.freq_df.loc[self.top_N_idx, :])
+
+        dgr = nx.degree_centrality(self.G)
+        self.dgr = sorted(dgr.items(), key=operator.itemgetter(1), reverse=True)
+
+        cls = nx.closeness_centrality(self.G)
+        self.cls = sorted(cls.items(), key=operator.itemgetter(1), reverse=True)
+
+        btw = nx.betweenness_centrality(self.G)
+        self.btw = sorted(btw.items(), key=operator.itemgetter(1), reverse=True)
+
+        eig = nx.eigenvector_centrality(self.G)
+        self.eig = sorted(eig.items(), key=operator.itemgetter(1), reverse=True)
+    
+    def plot(self, centrality='degree', title='Semantic Network Analysis', root='./'):
+        if centrality == 'degree':
+            title = title + ' (Degree)'
+            centrality_value = self.dgr
+        elif centrality == 'closeness':
+            title = title + ' (Closeness)'
+            centrality_value = self.cls
+        elif centrality == 'betweenness':
+            title = title + ' (Betweenness)'
+            centrality_value = self.btw
+        elif centrality == 'eigenvector':
+            title = title + ' (Eigenvector)'
+            centrality_value = self.eig
+
+        for token, central in centrality_value:
+            self.G.nodes[token]['weight'] = central
+
+        for token, relation_val in self.relation_map:
+            self.G.nodes[token]['relation'] = relation_val
+
+        fig = plt.figure(figsize=(30, 20))
+        plt.margins(x=0.1, y=0.2)
+        plt.rc('font', family='Malgun Gothic')
+        font_name = fm.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
+        rc('font', family=font_name)
+        ax = fig.gca()
+
+        # edge와 node의 label 부여하기
+        node_labels = dict((token, token) for token, _ in self.G.nodes(data=True))
+        try :
+            pos = graphviz_layout(self.G, prog='neato')
+        except : 
+            pos = nx.layout.fruchterman_reingold_layout(self.G)
+            # pos = nx.shell_layout(self.G)
+            # pos = nx.kamada_kawai_layout(self.G)
+            # pos = nx.random_layout(self.G)
+        nx.draw_networkx_labels(self.G, pos, font_family=font_name, labels=node_labels)
+
+        # Node에 centrality에 따라 weight 부여
+        Blues_modified = cm.get_cmap('Blues', 256)
+        bluescmp = ListedColormap(Blues_modified(np.linspace(0.2, 0.8, 4)))
+        nc_blues = nx.draw_networkx_nodes(self.G, pos,
+            nodelist = [node[0] for node in self.G.nodes(data=True) if node[1]['relation'] == self.keywords[0]], 
+            node_size=[node[1]["weight"]*20000 for node in self.G.nodes(data=True) if node[1]['relation'] == self.keywords[0]],
+            node_color=[node[1]['weight'] for node in self.G.nodes(data=True) if node[1]['relation'] == self.keywords[0]], 
+            cmap=bluescmp, node_shape='o', alpha=0.9, linewidths=0.4, edgecolors='#000000')
+
+        # Node에 centrality에 따라 weight 부여
+        Reds_modified = cm.get_cmap('Reds', 256)
+        redscmp = ListedColormap(Reds_modified(np.linspace(0.2, 0.8, 4)))
+        nc_reds = nx.draw_networkx_nodes(self.G, pos,
+            nodelist = [node[0] for node in self.G.nodes(data=True) if node[1]['relation'] == self.keywords[1]], 
+            node_size=[node[1]["weight"]*20000 for node in self.G.nodes(data=True) if node[1]['relation'] == self.keywords[1]],
+            node_color=[node[1]['weight'] for node in self.G.nodes(data=True) if node[1]['relation'] == self.keywords[1]], 
             cmap=redscmp, node_shape='o', alpha=0.9, linewidths=0.4, edgecolors='#000000')
 
         # Edge에 co-occurrence에 따라 weight 부여
